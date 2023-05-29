25d37266c4ed413602b18b26bed16de5a8f3f6b7
james goodloe <jag0007@tigermail.auburn.edu>
2023-05-29 14:30:54 -0500

    Create 16x16 grid

---
+++ script.js
@@ -0,0 +1,12 @@
+let grid_dim = 16;
+const draw_grid = document.querySelector('#draw-grid');
+for (i = 0; i < grid_dim; i++){
+    let new_row = document.createElement('div');
+    new_row.classList.add('grid-row');
+    for (j = 0; j < grid_dim; j++) {
+        let new_block = document.createElement('div');
+        new_block.classList.add('draw-block');
+        new_row.appendChild(new_block);
+    }
+    draw_grid.appendChild(new_row);
+}

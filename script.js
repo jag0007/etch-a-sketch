e6763f9de7dc5509edd2e11b17afeae69c163668
james goodloe <jag0007@tigermail.auburn.edu>
2023-05-29 17:18:47 -0500

    Move grid creation to a function

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let grid_dim = 16;
-const draw_grid = document.querySelector('#draw-grid');
-for (i = 0; i < grid_dim; i++){
-    let new_row = document.createElement('div');
-    new_row.classList.add('grid-row');
-    for (j = 0; j < grid_dim; j++) {
-        let new_block = document.createElement('div');
-        new_block.classList.add('draw-block');
-        new_row.appendChild(new_block);
+function make_grid(grid_dim) {
+    const draw_grid = document.querySelector('#draw-grid');
+    for (i = 0; i < grid_dim; i++){
+        let new_row = document.createElement('div');
+        new_row.classList.add('grid-row');
+        for (j = 0; j < grid_dim; j++) {
+            let new_block = document.createElement('div');
+            new_block.classList.add('draw-block');
+            new_row.appendChild(new_block);
+        }
+        draw_grid.appendChild(new_row);
     }
-    draw_grid.appendChild(new_row);
 }
+
+make_grid(16)
